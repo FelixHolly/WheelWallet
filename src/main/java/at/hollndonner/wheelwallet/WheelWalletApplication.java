d5ff497593a2ff73d888138f3d7e94d43ee2c5fb
felix <felixhollndonner@hotmail.com>
2023-06-02 19:59:58 +0200

    Init commit

---
+++ src/main/java/at/hollndonner/wheelwallet/WheelWalletApplication.java
@@ -0,0 +1,13 @@
+package at.hollndonner.wheelwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheelWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WheelWalletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/hollndonner/wheelwallet/WheelWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package at.hollndonner.wheelwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheelWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
